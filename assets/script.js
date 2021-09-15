e8d6be3fa76ec55fd7ec5eb085d972eee076a446
RobertCalder1 <robertcalder27@hotmail.com>
2021-09-15 21:29:45 +1000

    overhaul JS to make it cleaner and fix issue with being unable to proceed through questions, + added in scripting for local storage and clearing scores

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var highScoresButton = document.querySelector(".high-scores-button");
 var startButton = document.querySelector(".start-button");
 var answerButton = document.getElementsByClassName("answer-button");
 var backButton = document.querySelector(".back-button");
+var clearButton = document.querySelector(".clear-scores-button");
 //section variables
 var highScoresSection = document.querySelector(".high-scores-section");
 var startSection = document.querySelector(".start-section");
@@ -16,22 +17,44 @@ var timerElement = document.querySelector(".timer-count");
 var timer;
 var timerCount;
 //questions variables
-var questionsArray = [];
-var questionText = ["Hello", "World", "How", "Are", "You"];
-var questionOne = ["What", "When", "Why", "How"];
-var questionTwo = ["What", "When", "Why", "How"];
-var questionThree = ["What", "When", "Why", "How"];
-var questionFour = ["What", "When", "Why", "How"];
-var questionFive = ["What", "When", "Why", "How"];
+var questionsArray = [
+  {
+    question: "Hello",
+    answers: ["What", "When", "Why", "How"],
+    correct: "What",
+  },
+  {
+    question: "World",
+    answers: ["What", "When", "Why", "How"],
+    correct: "What",
+  },
+  {
+    question: "Lets",
+    answers: ["What", "When", "Why", "How"],
+    correct: "What",
+  },
+  {
+    question: "Have",
+    answers: ["What", "When", "Why", "How"],
+    correct: "What",
+  },
+  {
+    question: "Fun",
+    answers: ["What", "When", "Why", "How"],
+    correct: "What",
+  },
+];
+var questionIndex = 0;
 var questionTextElement = document.querySelector(".question-text");
 var answerBtnOne = document.querySelector(".answer-button-1");
 var answerBtnTwo = document.querySelector(".answer-button-2");
 var answerBtnThree = document.querySelector(".answer-button-3");
 var answerBtnFour = document.querySelector(".answer-button-4");
 var answerResult = document.querySelector(".answer-result");
-var questionAttempts;
 //other variables
 var score = 0;
+var highScoreRecords = document.querySelector(".score-records");
+var highScoresRecords = "";
 
 //create function for high score button
 function toggleHighScore() {
@@ -58,16 +81,10 @@ function startGame() {
   questionsSection.style.display = "inline-block";
   timerCount = 60;
   score = 0;
-  questionAttempts = 0;
-  startTimer();
-  questionsArray = ["q1", "q2", "q3", "q4", "q5"];
+  questionIndex = 0;
   shuffle(questionsArray);
-  shuffle(questionOne);
-  shuffle(questionTwo);
-  shuffle(questionThree);
-  shuffle(questionFour);
-  shuffle(questionFive);
-  question();
+  startTimer();
+  questions();
 }
 
 //create function for starting timer
@@ -88,126 +105,94 @@ function shuffle(array) {
 }
 
 //create function to show question and answers
-function question() {
-  if (questionsArray[0] === "q1") {
-    questionTextElement.innerHTML = questionText[0];
-    answerBtnOne.textContent = questionOne[0];
-    answerBtnTwo.textContent = questionOne[1];
-    answerBtnThree.textContent = questionOne[2];
-    answerBtnFour.textContent = questionOne[3];
-    questionsArray.shift();
-  } else if (questionsArray[0] === "q2") {
-    questionTextElement.innerHTML = questionText[1];
-    answerBtnOne.textContent = questionTwo[0];
-    answerBtnTwo.textContent = questionTwo[1];
-    answerBtnThree.textContent = questionTwo[2];
-    answerBtnFour.textContent = questionTwo[3];
-    questionsArray.shift();
-  } else if (questionsArray[0] === "q3") {
-    questionTextElement.innerHTML = questionText[2];
-    answerBtnOne.textContent = questionThree[0];
-    answerBtnTwo.textContent = questionThree[1];
-    answerBtnThree.textContent = questionThree[2];
-    answerBtnFour.textContent = questionThree[3];
-    questionsArray.shift();
-  } else if (questionsArray[0] === "q4") {
-    questionTextElement.innerHTML = questionText[3];
-    answerBtnOne.textContent = questionFour[0];
-    answerBtnTwo.textContent = questionFour[1];
-    answerBtnThree.textContent = questionFour[2];
-    answerBtnFour.textContent = questionFour[3];
-    questionsArray.shift();
-  } else if (questionsArray[0] === "q5") {
-    questionTextElement.innerHTML = questionText[4];
-    answerBtnOne.textContent = questionFive[0];
-    answerBtnTwo.textContent = questionFive[1];
-    answerBtnThree.textContent = questionFive[2];
-    answerBtnFour.textContent = questionFive[3];
-    questionsArray.shift();
+function questions() {
+  if (questionIndex < 5) {
+    var question = questionsArray[questionIndex];
+    shuffle(question.answers);
+    questionTextElement.innerHTML = question.question;
+    answerBtnOne.textContent = question.answers[0];
+    answerBtnTwo.textContent = question.answers[1];
+    answerBtnThree.textContent = question.answers[2];
+    answerBtnFour.textContent = question.answers[3];
+  } else {
+    endGame();
   }
 }
-
-//create function for answer
+//create function for answer selection
 function answer(event) {
-  if (questionsArray[0] === "q1") {
-    if (event.target.textContent === "What") {
-      correct();
-    } else {
-      incorrect();
-    }
+  var question = questionsArray[questionIndex];
+  if (event.target.textContent === question.correct) {
+    correct();
+  } else {
+    incorrect();
   }
-  if (questionsArray[0] === "q2") {
-    if (event.target.textContent === "What") {
-      correct();
-    } else {
-      incorrect();
-    }
-  }
-  if (questionsArray[0] === "q3") {
-    if (event.target.textContent === "What") {
-      correct();
-    } else {
-      incorrect();
-    }
-  }
-  if (questionsArray[0] === "q4") {
-    if (event.target.textContent === "What") {
-      correct();
-    } else {
-      incorrect();
-    }
-  }
-  if (questionsArray[0] === "q5") {
-    if (event.target.textContent === "What") {
-      correct();
-    } else {
-      incorrect();
-    }
-  }
+  questions();
 }
-//create answerResult functions
+//answerResult functions
 function correct() {
   answerResult.innerHTML = "Correct";
   score += 5;
   setTimeout(clearResult, 1000);
-  if (questionAttempts !== 5) {
-    questionAttempts++;
-    question();
-  } else {
-    endGame();
-  }
-  console.log(questionAttempts);
+  questionIndex++;
 }
 function incorrect() {
   answerResult.innerHTML = "Incorrect";
   timerCount -= 10;
   setTimeout(clearResult, 1000);
-  if (questionAttempts !== 5) {
-    questionAttempts++;
-    question();
-  } else {
-    endGame();
-  }
-  console.log(questionAttempts);
+  questionIndex++;
 }
+
 function clearResult() {
   answerResult.innerHTML = "";
 }
 
 function endGame() {
-  prompt("Score: " + score + "\nEnter Initials:");
+  setHighScore();
   toggleHighScore();
 }
 
+//runs on page load
+function init() {
+  getHighScores();
+}
+
+function setHighScore() {
+  var highScores = document.createTextNode(
+    prompt("Score: " + score + "\nEnter Initials:") + ": " + score
+  );
+  var createLi = document.createElement("li");
+  createLi.appendChild(highScores);
+  highScoreRecords.appendChild(createLi);
+  localStorage.setItem(
+    "highScoreRecords",
+    JSON.stringify(highScoreRecords.textContent)
+  );
+  console.log(localStorage.getItem("highScoreRecords"));
+}
+
+function getHighScores() {
+  var storedScores = localStorage.getItem("highScoreRecords");
+  var highScores = document.createTextNode(storedScores);
+  var createLi = document.createElement("li");
+  createLi.appendChild(highScores);
+  highScoreRecords.appendChild(createLi);
+  console.log(storedScores);
+}
+
+function clearHighScores() {
+  highScoreRecords.innerHTML = "";
+  localStorage.setItem("highScoreRecords", "");
+  console.log(localStorage.getItem("highScoreRecords"));
+}
+
 highScoresButton.addEventListener("click", toggleHighScore);
-
 startButton.addEventListener("click", startGame);
-
 backButton.addEventListener("click", goBack);
-
 startButton.addEventListener("click", startGame);
-
+clearButton.addEventListener("click", clearHighScores);
 answerButton[0].addEventListener("click", answer);
 answerButton[1].addEventListener("click", answer);
 answerButton[2].addEventListener("click", answer);
 answerButton[3].addEventListener("click", answer);
+
+init();
